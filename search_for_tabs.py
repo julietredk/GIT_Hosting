ede83a32f12c3b2153cc44a29d7368d9512e0f02
Yulia R <juletredk@gmail.com>
2023-02-27 14:02:33 +0000

    14

--- search_for_tabs.py
+++ search_for_tabs.py
@@ -4,7 +4,7 @@ import re
 for i in argv[1:]:
     file = open(i, 'r')
     result = file.read()
-    print(result)
+#    print(result)
     print("Length of the file is {}".format(len(result)))
     x = len(re.findall("\t", result))
     print("Number of TABS in file {} is {}".format(i, x))
